fd4d6c27e7def54b69c09b611e909deda7d4aba0
oppofind <836575280@qq.com>
2018-07-15 00:41:57 +0800

    modify comments

--- Common-util/src/main/java/com/power/common/model/CommonResult.java
+++ Common-util/src/main/java/com/power/common/model/CommonResult.java
@@ -4,102 +4,108 @@ import java.io.Serializable;
 
 /**
  * 公共返回结果
- * @author sunyu
  *
+ * @author sunyu
  */
 public class CommonResult<T> implements Serializable {
 
-	/**
-	 * serialVersionUID:.
-	 */
-	private static final long serialVersionUID = -7268040542410707954L;
+    /**
+     * serialVersionUID:.
+     */
+    private static final long serialVersionUID = -7268040542410707954L;
 
-	/**
-	 * 是否成功
-	 */
-	private boolean success = false;
+    /**
+     * 是否成功
+     */
+    private boolean success = false;
 
-	/**
-	 * 返回信息
-	 */
-	private String message;
+    /**
+     * 响应信息
+     */
+    private String message;
 
-	/**
-	 * 装在数据
-	 */
-	private T data;
+    /**
+     * 响应数据
+     */
+    private T data;
 
-	/**
-	 * 错误代码
-	 */
-	private String code;
+    /**
+     * 错误代码
+     */
+    private String code;
 
-	/**
-	 * 默认构造器
-	 */
-	public CommonResult() {
+    /**
+     * 默认构造器
+     */
+    public CommonResult() {
 
-	}
+    }
 
-	/**
-	 * @param success 是否成功
-	 * @param message 返回的消息
-	 */
-	public CommonResult(boolean success, String message) {
-		this.success = success;
-		this.message = message;
-	}
+    /**
+     * @param success 是否成功
+     * @param message 返回的消息
+     */
+    public CommonResult(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
 
-	/**
-	 * @param success 是否成功
-	 */
-	public CommonResult(boolean success) {
-		this.success = success;
-	}
+    /**
+     * @param success 是否成功
+     */
+    public CommonResult(boolean success) {
+        this.success = success;
+    }
 
-	/**
-	 * @param code    error code
-	 * @param message success or error messages
-	 */
-	public CommonResult(String code, String message) {
-		this.code = code;
-		this.message = message;
-	}
+    /**
+     * @param code    error code
+     * @param message success or error messages
+     */
+    public CommonResult(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 
-	/**
-	 * @param success 是否成功
-	 * @param message 消息
-	 * @param data    数据
-	 */
-	public CommonResult(boolean success, String message, T data) {
-		this.success = success;
-		this.message = message;
-		this.data = data;
-	}
-	public boolean isSuccess() {
-		return success;
-	}
-	public void setSuccess(boolean success) {
-		this.success = success;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public T getData() {
-		return data;
-	}
-	public void setData(T data) {
-		this.data = data;
-	}
+    /**
+     * @param success 是否成功
+     * @param message 消息
+     * @param data    数据
+     */
+    public CommonResult(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
 
-	public String getCode() {
-		return code;
-	}
+    public boolean isSuccess() {
+        return success;
+    }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
--- Common-util/src/main/java/com/power/common/model/PageVo.java
+++ Common-util/src/main/java/com/power/common/model/PageVo.java
@@ -10,147 +10,177 @@ import java.util.List;
  * USAGE:
  * RequestMapping(value="news/p{pageIndex}.htm",method = RequestMethod.GET)
  * public String list(@PathVariable int pageIndex,Model model){
- * 	try{
- *      PageVo pageDto = this.newsService.getPage(0,pageIndex,12);
- *      pageDto.setUrl("news/p{0}.htm");
- *      model.addAttribute("list",pageDto);
- *	}catch (Exception e){
- *		 return "";
- *	}
- *	return "forward:/news.jsp";
- *}
- * @author sunyu
+ * try{
+ * PageVo pageDto = this.newsService.getPage(0,pageIndex,12);
+ * pageDto.setUrl("news/p{0}.htm");
+ * model.addAttribute("list",pageDto);
+ * }catch (Exception e){
+ * return "";
+ * }
+ * return "forward:/news.jsp";
+ * }
+ *
  * @param <T> Generics
+ * @author sunyu
  */
 public class PageVo<T> implements Serializable {
-	private static final long serialVersionUID = 1923401118856169487L;
-	private Long total;//total record
-	private String url;//使用messageFormat占位替换
-	private Integer pageIndex;// 页码,当前页数
-	private Integer pageSize;// 分页条数
-	private List<T> items;// 查询结果
-	private int displayEntries = 5;//连续分页主体部分显示的分页条目数
-	private int edgeEntries = 1;//两侧显示的首尾分页的条目数
+    private static final long serialVersionUID = 1923401118856169487L;
 
-	//default
-	public PageVo() {
-		super();
-	}
+    /**
+     * total record
+     */
+    private Long total;
 
-	public PageVo(Long total, Integer pageIndex, Integer pageSize, List<T> items) {
-		super();
-		this.total = total;
-		this.pageIndex = pageIndex;
-		this.pageSize = pageSize;
-		this.items = items;
-	}
+    /**
+     * 使用messageFormat占位替换
+     */
+    private String url;
 
-	public Long getTotal() {
-		return total;
-	}
+    /**
+     * 页码,当前页数
+     */
+    private Integer pageIndex;
 
-	public void setTotal(Long total) {
-		this.total = total;
-	}
+    /**
+     * 分页条数
+     */
+    private Integer pageSize;
 
-	public Integer getPageIndex() {
-		return pageIndex;
-	}
+    /**
+     * 查询结果
+     */
+    private List<T> items;
 
-	public void setPageIndex(Integer pageIndex) {
-		this.pageIndex = pageIndex;
-	}
+    /**
+     * 连续分页主体部分显示的分页条目数
+     */
+    private int displayEntries = 5;
 
-	public Integer getPageSize() {
-		return pageSize;
-	}
+    /**
+     * 两侧显示的首尾分页的条目数
+     */
+    private int edgeEntries = 1;
 
-	public void setPageSize(Integer pageSize) {
-		this.pageSize = pageSize;
-	}
+    //default
+    public PageVo() {
+        super();
+    }
 
-	public List<T> getItems() {
-		return items;
-	}
+    public PageVo(Long total, Integer pageIndex, Integer pageSize, List<T> items) {
+        super();
+        this.total = total;
+        this.pageIndex = pageIndex;
+        this.pageSize = pageSize;
+        this.items = items;
+    }
 
-	public void setItems(List<T> items) {
-		this.items = items;
-	}
+    public Long getTotal() {
+        return total;
+    }
 
-	@Override
-	public String toString() {
-		return "PageVo [total=" + total + ", pageIndex=" + pageIndex
-				+ ", pageSize=" + pageSize + ", items=" + items + "]";
-	}
+    public void setTotal(Long total) {
+        this.total = total;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public Integer getPageIndex() {
+        return pageIndex;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getNavigation(){
-		Integer totalPage = (int) (total % pageSize > 0 ? (total
-				/ pageSize + 1) : (total / pageSize));
-		//From jQuery pagination
-		//int ne_half = ((displayEntries&1)==1)?(displayEntries/2+1):displayEntries/2;
-		int ne_half = ((displayEntries&1)==1)?(displayEntries>>1)+1:displayEntries>>1;
-		int upper_limit = totalPage-displayEntries;
-		pageIndex = (pageIndex<1)?1:pageIndex;
-		int currentPage = pageIndex -1;
+    public void setPageIndex(Integer pageIndex) {
+        this.pageIndex = pageIndex;
+    }
 
-		int start = currentPage>ne_half?Math.max(Math.min(currentPage-ne_half, upper_limit), 0):0;
-		int end = currentPage>ne_half?Math.min(currentPage+ne_half,totalPage):Math.min(displayEntries,totalPage);
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("<div	id=\"pagination\" class=\"pagination\">");
-		// Generate "Previous"-Link
-		if(pageIndex==1||totalPage==0){
-			buffer.append("<span class=\"disabled\">上一页</span>");
-		}else{
-			String str = MessageFormat.format("<a href=\""+this.url+"\"",pageIndex-1);
-			buffer.append(str).append(">上一页</a>");
-		}
-		// Generate starting points
-		if(start>0&&edgeEntries>0){
-			int end1 = Math.min(edgeEntries, start);
-			for(int i=0; i<end1; i++) {
-				String str = MessageFormat.format("<a href=\""+this.url+"\"",i+1);
-				buffer.append(str).append(">").append(i+1).append("</a>");
-			}
-			if(edgeEntries < start) {
-				buffer.append("<span>...</span>");
-			}
-		}
-		// Generate interval links
-		for(int i= start; i<end; i++) {
-			if(i+1==pageIndex){
-				//current page
-				buffer.append("<span class=\"current\">").append(i+1).append("</span>");
-			}else{
-				String str = MessageFormat.format("<a href=\""+this.url+"\"",i+1);
-				buffer.append(str).append(">").append(i+1).append("</a>");
-			}
-		}
-		// Generate ending points
-		if (end < totalPage &&edgeEntries > 0) {
-			if(totalPage-edgeEntries > end) {
-				buffer.append("<span>...</span>");
-			}
-			int begin = Math.max(totalPage-edgeEntries, end);
-			for(int i=begin; i<totalPage; i++) {
-				String str = MessageFormat.format("<a href=\""+this.url+"\"",i+1);
-				buffer.append(str).append(">").append(i+1).append("</a>");
-			}
-		}
-		// Generate "Next"-Link
-		if(pageIndex == totalPage || totalPage == 0){
-			buffer.append("<span class=\"disabled\">下一页</span>");
-		}else{
-			String str = MessageFormat.format("<a href=\""+this.url+"\"",pageIndex+1);
-			buffer.append(str).append(">下一页</a>");
-		}
-		buffer.append("</div>");
-		return buffer.toString();
-	}
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+    public void setItems(List<T> items) {
+        this.items = items;
+    }
+
+    @Override
+    public String toString() {
+        return "PageVo [total=" + total + ", pageIndex=" + pageIndex
+                + ", pageSize=" + pageSize + ", items=" + items + "]";
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getNavigation() {
+        Integer totalPage = (int) (total % pageSize > 0 ? (total
+                / pageSize + 1) : (total / pageSize));
+        //From jQuery pagination
+        //int ne_half = ((displayEntries&1)==1)?(displayEntries/2+1):displayEntries/2;
+        int ne_half = ((displayEntries & 1) == 1) ? (displayEntries >> 1) + 1 : displayEntries >> 1;
+        int upper_limit = totalPage - displayEntries;
+        pageIndex = (pageIndex < 1) ? 1 : pageIndex;
+        int currentPage = pageIndex - 1;
+
+        int start = currentPage > ne_half ? Math.max(Math.min(currentPage - ne_half, upper_limit), 0) : 0;
+        int end = currentPage > ne_half ? Math.min(currentPage + ne_half, totalPage) : Math.min(displayEntries, totalPage);
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("<div	id=\"pagination\" class=\"pagination\">");
+        // Generate "Previous"-Link
+        if (pageIndex == 1 || totalPage == 0) {
+            buffer.append("<span class=\"disabled\">上一页</span>");
+        } else {
+            String str = MessageFormat.format("<a href=\"" + this.url + "\"", pageIndex - 1);
+            buffer.append(str).append(">上一页</a>");
+        }
+        // Generate starting points
+        if (start > 0 && edgeEntries > 0) {
+            int end1 = Math.min(edgeEntries, start);
+            for (int i = 0; i < end1; i++) {
+                String str = MessageFormat.format("<a href=\"" + this.url + "\"", i + 1);
+                buffer.append(str).append(">").append(i + 1).append("</a>");
+            }
+            if (edgeEntries < start) {
+                buffer.append("<span>...</span>");
+            }
+        }
+        // Generate interval links
+        for (int i = start; i < end; i++) {
+            if (i + 1 == pageIndex) {
+                //current page
+                buffer.append("<span class=\"current\">").append(i + 1).append("</span>");
+            } else {
+                String str = MessageFormat.format("<a href=\"" + this.url + "\"", i + 1);
+                buffer.append(str).append(">").append(i + 1).append("</a>");
+            }
+        }
+        // Generate ending points
+        if (end < totalPage && edgeEntries > 0) {
+            if (totalPage - edgeEntries > end) {
+                buffer.append("<span>...</span>");
+            }
+            int begin = Math.max(totalPage - edgeEntries, end);
+            for (int i = begin; i < totalPage; i++) {
+                String str = MessageFormat.format("<a href=\"" + this.url + "\"", i + 1);
+                buffer.append(str).append(">").append(i + 1).append("</a>");
+            }
+        }
+        // Generate "Next"-Link
+        if (pageIndex == totalPage || totalPage == 0) {
+            buffer.append("<span class=\"disabled\">下一页</span>");
+        } else {
+            String str = MessageFormat.format("<a href=\"" + this.url + "\"", pageIndex + 1);
+            buffer.append(str).append(">下一页</a>");
+        }
+        buffer.append("</div>");
+        return buffer.toString();
+    }
 }
