8e8de13e719169fe568c8663d9002ad614b6b004
oppofind <836575280@qq.com>
2018-10-22 19:46:38 +0800

    add uuid util

---
+++ Common-util/src/main/java/com/power/common/util/UUIDUtil.java
@@ -0,0 +1,26 @@
+package com.power.common.util;
+
+import java.util.UUID;
+
+/**
+ * @author yu 2018/10/14.
+ */
+public class UUIDUtil {
+
+
+    /**
+     * uuid
+     * @return string
+     */
+    public static String getUuid(){
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * 32位uuid
+     * @return string
+     */
+    public static String getUuid32(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+}
