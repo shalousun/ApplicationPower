7a90a6718cde21e4e6081c2bd1df94133a11db85
oppofind <836575280@qq.com>
2019-01-21 09:36:07 +0800

    Add BaseRuntimeException

---
+++ Common-util/src/main/java/com/power/common/exception/BaseRuntimeException.java
@@ -0,0 +1,38 @@
+package com.power.common.exception;
+
+import com.power.common.interfaces.IMessage;
+
+/**
+ * @author yu 2019/1/21.
+ */
+public class BaseRuntimeException extends RuntimeException implements IMessage {
+
+    private String  errorCode;
+
+    protected BaseRuntimeException(IMessage iMessage){
+        super(iMessage.getMessage());
+        this.errorCode = iMessage.getCode();
+    }
+
+    protected BaseRuntimeException(IMessage iMessage, String message){
+        super(message);
+        this.errorCode = iMessage.getCode();
+    }
+
+
+    protected BaseRuntimeException(String errorCode, String message){
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+
+    @Override
+    public String getCode() {
+        return this.errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
