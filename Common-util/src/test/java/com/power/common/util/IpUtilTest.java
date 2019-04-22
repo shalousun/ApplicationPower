1dde450c7b51bd21e18af5acb6f9cc6f1e0106f1
oppofind <836575280@qq.com>
2019-04-22 09:48:06 +0800

    add IpUtil unit test

---
+++ Common-util/src/test/java/com/power/common/util/IpUtilTest.java
@@ -0,0 +1,14 @@
+package com.power.common.util;
+
+import org.junit.Test;
+
+/**
+ * @author yu 2019/4/22.
+ */
+public class IpUtilTest {
+
+    @Test
+    public void testGetIpAddress(){
+
+    }
+}
