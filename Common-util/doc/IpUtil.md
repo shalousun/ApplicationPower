7267cbf8c4204a596f30b0d372e0a4474e90f440
oppofind <836575280@qq.com>
2019-04-25 09:40:32 +0800

    upgrade common-util module

--- Common-util/doc/IpUtil.md
+++ Common-util/doc/IpUtil.md
@@ -9,3 +9,20 @@ Usage:
 ```
 IpUtil.getIpAddr(request);//return 192.168.15.80
 ```
+## 2. getLocalIPV4()方法
+
+该方法用于返回服务器所在机器所有IPV4地址,map中的key是网卡名称，value是ip地址
+
+Usage:
+```
+Map<String,String> ipsMap = IpUtil.getLocalIPV4();
+```
+
+## 3. getLocalIPV6()方法
+
+该方法用于返回服务器所在机器所有IPV6地址,map中的key是网卡名称，value是ip地址
+
+Usage:
+```
+Map<String,String> ipsMap = IpUtil.getLocalIPV6();
+```
--- Common-util/src/main/java/com/power/common/util/IpUtil.java
+++ Common-util/src/main/java/com/power/common/util/IpUtil.java
@@ -11,97 +11,101 @@ import java.util.Map;
 
 public class IpUtil {
 
-	private static String serverIp;
+    private static String serverIp;
 
-	static {
-		InetAddress ia = null;
-		try {
-			ia = ia.getLocalHost();
-			serverIp = ia.getHostAddress();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    static {
+        InetAddress ia = null;
+        try {
+            ia = ia.getLocalHost();
+            serverIp = ia.getHostAddress();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * 获取ip地址
-	 * @param request HttpServletRequest
-	 * @return string
+    /**
+     * 获取ip地址
+     *
+     * @param request HttpServletRequest
+     * @return string
      */
-	public static String getIpAddr(HttpServletRequest request) {
-		String ip = request.getHeader("x-forwarded-for");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("WL-Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getRemoteAddr();
-		}
-		//cast localhost
-		if("0:0:0:0:0:0:0:1".equals(ip)){
-			ip = getServerIp();
-		}
-		return ip;
-	}
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        //cast localhost
+        if ("0:0:0:0:0:0:0:1".equals(ip)) {
+            ip = getServerIp();
+        }
+        return ip;
+    }
 
 
-	/**
-	 * 获取服务器的ip地址
-	 * @return String
-	 */
-	public static String getServerIp() {
-		return serverIp;
-	}
+    /**
+     * 获取服务器的ip地址
+     *
+     * @return String
+     */
+    public static String getServerIp() {
+        return serverIp;
+    }
 
-	/**
-	 * 获取所有的网卡的ip v4地址,key为网卡地址，value为ip地址
-	 * @return
-	 */
-	public static Map<String,String> getLocalIPV4(){
-		Map<String,String> map = new HashMap<>();
-		InetAddress ip = null;
-		try {
-			Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
-			while (netInterfaces.hasMoreElements()) {
-				NetworkInterface ni = netInterfaces.nextElement();
-				Enumeration<InetAddress> ips = ni.getInetAddresses();
-				while (ips.hasMoreElements()) {
-					ip = ips.nextElement();
-					if(ip instanceof Inet4Address){
-						map.put(ni.getName(),ip.getHostAddress());
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return map;
-	}
+    /**
+     * 获取所有的网卡的ip v4地址,key为网卡地址，value为ip地址
+     *
+     * @return hash map
+     */
+    public static Map<String, String> getLocalIPV4() {
+        Map<String, String> map = new HashMap<>();
+        InetAddress ip = null;
+        try {
+            Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (netInterfaces.hasMoreElements()) {
+                NetworkInterface ni = netInterfaces.nextElement();
+                Enumeration<InetAddress> ips = ni.getInetAddresses();
+                while (ips.hasMoreElements()) {
+                    ip = ips.nextElement();
+                    if (ip instanceof Inet4Address) {
+                        map.put(ni.getName(), ip.getHostAddress());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
 
-	/**
-	 * 获取所有ipv6地址
-	 * @return
-	 */
-	public static Map<String,String> getLocalIPV6(){
-		Map<String,String> map = new HashMap<>();
-		InetAddress ip = null;
-		try {
-			Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
-			while (netInterfaces.hasMoreElements()) {
-				NetworkInterface ni = netInterfaces.nextElement();
-				Enumeration<InetAddress> ips = ni.getInetAddresses();
-				while (ips.hasMoreElements()) {
-					ip = ips.nextElement();
-					if(ip instanceof Inet6Address){
-						map.put(ni.getName(),ip.getHostAddress());
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return map;
-	}
+    /**
+     * 获取所有ipv6地址
+     *
+     * @return hash map
+     */
+    public static Map<String, String> getLocalIPV6() {
+        Map<String, String> map = new HashMap<>();
+        InetAddress ip = null;
+        try {
+            Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (netInterfaces.hasMoreElements()) {
+                NetworkInterface ni = netInterfaces.nextElement();
+                Enumeration<InetAddress> ips = ni.getInetAddresses();
+                while (ips.hasMoreElements()) {
+                    ip = ips.nextElement();
+                    if (ip instanceof Inet6Address) {
+                        map.put(ni.getName(), ip.getHostAddress());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
 }
