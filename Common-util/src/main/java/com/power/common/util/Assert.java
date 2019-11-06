22492ef6c25977153e4fd529ecbcb49940131b45
oppofind <836575280@qq.com>
2019-11-06 10:23:44 +0800

    add comments

--- Common-util/src/main/java/com/power/common/util/Assert.java
+++ Common-util/src/main/java/com/power/common/util/Assert.java
@@ -7,6 +7,12 @@ import com.power.common.exception.AssertException;
  */
 public class Assert {
 
+    /**
+     * Assert null
+     * @param object object
+     * @param message message
+     * @param args args
+     */
     public static void notNull(Object object, String message,Object... args ) {
         if (object == null) {
             throw new AssertException(String.format(message, args));
