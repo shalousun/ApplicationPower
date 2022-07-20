d47c00b8b595944161f194c53faa2daf9b941c97
shalousun <smartdoc647@gmail.com>
2022-07-20 14:29:16 +0800

    add: ReflectionUtil

---
+++ Common-util/src/main/java/com/power/common/util/ReflectionUtil.java
@@ -0,0 +1,27 @@
+package com.power.common.util;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author yu on 2022/7/20
+ */
+public class ReflectionUtil {
+
+  /**
+   * Retrieving Fields, Contains Inherited Fields
+   * @param clazz Class
+   * @return Array of Field
+   */
+  public static Field[] getAllFields(Class<?> clazz) {
+    List<Field> fieldList = new ArrayList<>();
+    while (clazz != null){
+      fieldList.addAll(new ArrayList<>(Arrays.asList(clazz.getDeclaredFields())));
+      clazz = clazz.getSuperclass();
+    }
+    Field[] fields = new Field[fieldList.size()];
+    return fieldList.toArray(fields);
+  }
+}
